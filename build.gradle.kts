749020ccd302119f184ca60273ce8018503afbcb
Stephen Cerruti <scerruti@miracosta.edu>
2025-11-16 21:25:42 -0800

    Consolidate publish workflows: remove publish-to-maven-central.yml, set nmcp plugin to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("signing")
-    id("io.github.gradleup.nmcp") version "latest"
+    id("io.github.gradleup.nmcp") version "1.1.0"
 }
 
 // Ensure a consistent Java toolchain for local and CI builds. This makes Gradle
