a02f5a60cab45c3b257d046b9a2fb71ca1dce85f
Stephen Cerruti <scerruti@miracosta.edu>
2025-11-17 13:27:23 -0800

    Cleanup settings.gradle.kts: remove old comments and reorder rootProject.name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,6 @@ pluginManagement {
         mavenCentral()
     }
 }
-rootProject.name = "JCoDroneEdu"
 
-// Include the flight-patterns module as a separate publishable component
-include("flight-patterns")
+rootProject.name = "JCoDroneEdu"
 
